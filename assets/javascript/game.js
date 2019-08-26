719fff343e141e30f1201d505f3e659400ab2efc
Patrick ONeill <poneill@sembracare.com>
2019-08-26 19:22:22 -0400

    adding extra comments to explain program flow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,52 +33,72 @@ var topTVObject = {
 }    
 
 window.onload = function() {
+    //when the window loads pick a random word
     topTVObject.pickRandomWord();
 };
 
 document.onkeyup = function(e) {
+    //sets the document span element "guessesRemaining" to the variable guessesRemaining
     var guessesRemaining = document.getElementById("guessesRemaining");
 
     // Determines which key was pressed.
     var userGuess = e.key;
+    
+    //dynamically gets letter elementID on html page and sets element to var letter
     var letterString = "letter" + userGuess.toUpperCase();
     var letter = document.getElementById(letterString);
 
+    //check if letter has already been picked    
     if(!letter.classList.contains("letterPicked")) {
+        //add class letterPicked which highlights the letter on page
         letter.classList.add("letterPicked");
+        //remove one guess from guessesRemaining
         guessesRemaining.textContent = parseInt(guessesRemaining.textContent) - 1;
 
+        //checks if letter was in the answer string
         if(pickedWord.toLowerCase().indexOf(userGuess) != -1) {
+            //plays correct sound when letter is correct
             document.getElementById("rightSound").play();
+
+            //sets userGuessIndex array
             var userGuessIndex = [];
+            //loops through correct word to find all indexes of userGuess
             for(var i=0; i<pickedWord.length;i++) {
                 if (pickedWord[i].toLowerCase() === userGuess) userGuessIndex.push(i);
             }
 
+            //loops through userGuessIndex array and replaces currentGuess on screen with correct letters
             userGuessIndex.forEach(function(element) {
                 var currentGuess = document.getElementById("gameWord").textContent; 
                 currentGuess = currentGuess.substr(0, element) + pickedWord[element] + currentGuess.substr(element + 1);
                 document.getElementById("gameWord").textContent = currentGuess;
             });
         } else {
+            //plays the wrong Sound on a wrong answer
             document.getElementById("wrongSound").play();
         }
     }
 
+    //checks if user Won the game
     if(document.getElementById("gameWord").textContent === pickedWord.replace(/ /g,"+")) 
     {  
+        //plays winning Sound when pickedword matches gameWord
         document.getElementById("winSound").play();
         alert("You Win!");
+        //adds one Win to total
         document.getElementById("winsTotal").textContent = parseInt(document.getElementById("winsTotal").textContent) + 1;
+        //changes the Image, Name, Detail, and Link 
         document.getElementById("tvImage").src = topTVObject.imgSource[randomIndex];
         document.getElementById("tvName").textContent = topTVObject.name[randomIndex];
         document.getElementById("tvDetail").textContent = topTVObject.detail[randomIndex];
         document.getElementById("tvLink").href = topTVObject.link[randomIndex];
+        //resets the game
         gameReset();
     }
 
     if(guessesRemaining.textContent === "0")
     {
+        //if guessesRemaining equal zero then play game Lose sound and reset the game
         document.getElementById("loseSound").play();
         alert("You Lose!");
         gameReset();
@@ -87,11 +107,12 @@ document.onkeyup = function(e) {
 
   function gameReset() {
     var pickedLetters = document.getElementsByClassName('letterPicked');    
-       
+    //removed letterPicked CSS class from each element with class letterPicked
     while(pickedLetters.length > 0){
         pickedLetters[0].classList.remove('letterPicked');
     }
-
+    //reset guessesRemaining to 13
     guessesRemaining.textContent = "13";
+    //pick a new random word
     topTVObject.pickRandomWord();
   }
